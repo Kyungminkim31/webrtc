54cabb6d894da4eab0bc448d506f4fbb23b34cf5
Kyungmin Kim <kyungmin.kim.31@gmail.com>
2020-02-12 16:58:17 +0900

    Add process to save pre picking frames

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ let recordedBlobs;
 let mediaRecorder;
 let sourceBuffer;
 
+let tmpPicture, tmpContext;
+let preRollPictures;
+
+const MAX_LIMIT = 5;
+
 var isChannelReady = false;
 var isInitiator = false;
 var isStarted = false;
@@ -15,12 +20,16 @@ var remoteStream;
 var turnReady;
 
 var recordedVideo = document.getElementById('recordedVideo');
+var recordBtn = document.getElementById('recordBtn');
 var sendBtn = document.getElementById('sendBtn');
 var playBtn = document.getElementById('playBtn');
 var snapBtn = document.getElementById('snapBtn');
 var uploadBtn = document.getElementById('uploadBtn');
 var dataChannelSend = document.querySelector('input#dataChannelSend');
 var dataChannelReceive = document.querySelector('textarea#dataChannelReceive');
+var statusContainer = document.getElementById('statusSpan');
+var pickingBtn = document.getElementById('pickingBtn');
+var scrollDiv = document.getElementById('scrollCanvas');
 
 var receiveChannel;
 var sendChannel;
@@ -33,7 +42,7 @@ var photoContextH;
 
 var pcConfig = {
   'iceServers': [{
-    'urls': 'stun:stun.l.google.com:19302'
+	'urls': 'stun:stun.l.google.com:19302'
   }]
 };
 
@@ -51,21 +60,23 @@ playBtn.addEventListener('click', () => {
   const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
   recordedVideo.src = null;
   recordedVideo.srcObject = null;
+  recordedVideo.style.visibility = 'visible';
   recordedVideo.src = window.URL.createObjectURL(superBuffer);
   recordedVideo.controls = true;
   recordedVideo.play();
+  playBtn.disabled = true;
 });
 
 sendBtn.addEventListener('click', ()=>{
   var data = dataChannelSend.value;
   try{
-    sendChannel.send(data);
-    dataChannelReceive.value += data +'\n';
+	sendChannel.send(data);
+	dataChannelReceive.value += data +'\n';
   } catch(err){
-    console.log('Something is going to be wrong...' + err.name);
+	console.log('Something is going to be wrong...' + err.name);
   }
-    trace('sent data : '+data);
-    dataChannelSend.value ='';
+	trace('sent data : '+data);
+	dataChannelSend.value ='';
 });
 
 snapBtn.addEventListener('click', ()=>{
@@ -74,6 +85,8 @@ snapBtn.addEventListener('click', ()=>{
   photo.style.visibility = "visible"; 
   trace('draw image on canvas...');  
 });
+
+// upload event handler
 var xhr;
 uploadBtn.addEventListener('click', ()=>{
   trace('uploadSnampshot...');
@@ -86,14 +99,13 @@ uploadBtn.addEventListener('click', ()=>{
   // do some Ajax...
   xhr = new XMLHttpRequest();
   xhr.onreadystatechange = viewMessage;
-  //xhr.open('POST', '/upload_data.php',true);
-  xhr.open('POST', 'http://node.visionconnect.co.kr/savex', true);
+  xhr.open('POST', 'http://node.visionconnect.co.kr:4000/uploadx', true);
 
   xhr.upload.onprogress = function(e) {
-    if(e.lengthComputable){
-      var percentComplete = (e.loaded/e.total) * 100;
-      trace(percentComplete + '% upload');
-    }
+	if(e.lengthComputable){
+	  var percentComplete = (e.loaded/e.total) * 100;
+	  trace(percentComplete + '% upload');
+	}
   };
 
   xhr.onload = function(){};
@@ -108,6 +120,48 @@ snapBtn.disabled = true;
 uploadBtn.disabled = true;
 dataChannelSend.disabled = true;
 stopBtn.disabled = true;
+playBtn.disabled = true;
+recordBtn.disabled = false;
+
+
+/////////////////////////////////////
+// setting functions 
+/////////////////////////////////////
+// 픽업 이벤트 발생전까지 5개의 스냅샵을 유지한다.
+function makePreRecordedpreRollPictures() {
+
+  tmpPicture = document.createElement('canvas');
+  tmpContext = tmpPicture.getContext('2d');
+
+  tmpPicture.width = tmpContext.width = localVideo.videoWidth;
+  tmpPicture.height = tmpContext.width = localVideo.videoHeight;
+
+  // tmpPicture.width = tmpContext.width = remoteVideo.videoWidth;
+  // tmpPicture.height = tmpContext.width = remoteVideo.videoHeight;
+
+  tmpContext.drawImage(localVideo, 0, 0, tmpPicture.width, tmpPicture.height);
+  // tmpContext.drawImage(remoteVideo, 0, 0, tmpPicture.width, tmpPicture.height);
+
+  if(preRollPictures.length === MAX_LIMIT){
+	preRollPictures.shift();
+  }
+  preRollPictures.push(tmpPicture);
+}
+
+// 지정된 사이즈 만큼의 Snapshot을 만들어 낸다.
+function drawPhotos(){  
+  for(var i = 0; i < preRollPictures.length;i++){
+	preRollPictures[i].style.visibility = 'visible';
+	preRollPictures[i].style.width ="120px";
+	preRollPictures[i].style.height ="auto";
+	preRollPictures[i].style.padding ="5px";
+	preRollPictures[i].style.display ="inline-block";
+	scrollDiv.style.border = "1px solid #ccc";
+	scrollDiv.style.overflow = "auto";
+	scrollDiv.style.whiteSpace = "nowrap";
+	scrollDiv.appendChild(preRollPictures[i]);
+  }
+}
 
 function handleSourceOpen(event) {
   console.log('MediaSource opened');
@@ -116,47 +170,59 @@ function handleSourceOpen(event) {
 }
 
 function handleDataAvailable(event){
-  console.log('handleDataAvailable', event);
+  //console.log('handleDataAvailable', event);
   if(event.data && event.data.size > 0){
-    recordedBlobs.push(event.data);
+	recordedBlobs.push(event.data);
   }
 }
 
 function beginRecording(){
+	preRollPictures = [];
+  setInterval(makePreRecordedpreRollPictures, 1000);
   trace('>>>>> on Record <<<<<');
   stopBtn.disabled = false;
+  playBtn.disabled = true;
+
   // stopBtn.style.visibility = 'visible';
+  recordedVideo.src = null;
+  recordedVideo.srcObject = null;
+  recordedVideo.style.visibility = 'hidden';
 
   recordedBlobs = [];
+
   let options = {mimeType: 'video/webm;codecs=vp9'};
   if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-    console.error(`${options.mimeType} is not Supported`);
-    // errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
-    options = {mimeType: 'video/webm;codecs=vp8'};
-    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-      console.error(`${options.mimeType} is not Supported`);
-      // errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
-      options = {mimeType: 'video/webm'};
-      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-        console.error(`${options.mimeType} is not Supported`);
-        // errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
-        options = {mimeType: ''};
-      }
-    }
+  	console.error(`${options.mimeType} is not Supported`);
+  	// errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
+  	options = {mimeType: 'video/webm;codecs=vp8'};
+  	if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+  	  console.error(`${options.mimeType} is not Supported`);
+  	  // errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
+  	  options = {mimeType: 'video/webm'};
+  	  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+  		console.error(`${options.mimeType} is not Supported`);
+  		// errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
+  		options = {mimeType: ''};
+  	  }
+  	}
   }
 
   try {
-    mediaRecorder = new MediaRecorder(localStream, options);
+  	// mediaRecorder = new MediaRecorder(remoteStream, options);
+  	mediaRecorder = new MediaRecorder(localStream, options);
   } catch (e) {
-      console.error('Exception while creating MediaRecorder:', e);
-      // errorMsgElement.innerHTML = 'Exception while creating MediaRecorder:  ${JSON.stringify(e)}`;
-      return;
+	  console.error('Exception while creating MediaRecorder:', e);
+	  // errorMsgElement.innerHTML = 'Exception while creating MediaRecorder:  ${JSON.stringify(e)}`;
+	  return;
   }
 
   console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
   mediaRecorder.onstop = (event) => {
-    console.log('Recorder stopped:', event);
-    console.log('Recorded Blobs: ', recordedBlobs);
+  	console.log('Recorder stopped:', event);
+  	console.log('Recorded Blobs: ', recordedBlobs);
+    console.log('preRollPictures [] : ',preRollPictures);
+    
+    drawPhotos();
   };
   mediaRecorder.ondataavailable = handleDataAvailable;
   mediaRecorder.start(10);
@@ -164,9 +230,10 @@ function beginRecording(){
 }
 
 function stopRecording(){
-  trace('<<<< Stop recording >>>>');
+  clearInterval();
+  console.log('<<<< Stop recording >>>>');
   stopBtn.disabled = true;
-  // stopBtn.style.visibility = 'hidden';
+  playBtn.disabled = false;
   mediaRecorder.stop();
 }
 
@@ -179,14 +246,14 @@ function snapPhoto(){
 
 function viewMessage(){
   if(xhr.readyState==4){
-    if(xhr.status===200){
-      var responseObj = JSON.parse(xhr.response);
-      if(responseObj.value === 1){
-        photo.style.visibility = "hidden"; 
-        // 'uploading is sucess'
-        alert('Uploading is sucess.');
-      }
-    }
+	if(xhr.status===200){
+	  var responseObj = JSON.parse(xhr.response);
+	  if(responseObj.value === "1"){
+		photo.style.visibility = "hidden"; 
+		// 'uploading is sucess'
+		alert('Uploading is sucess.');
+	  }
+	}
   }
 }
 
@@ -242,23 +309,23 @@ socket.on('answer', function(message){
 socket.on('message', function(message) {
   console.log('Client received message:', message);
   if (message === 'got user media') {
-    maybeStart();
+	maybeStart();
   } else if (message.type === 'offer') {
-    if (!isInitiator && !isStarted) {
-      maybeStart();
-    }
-    pc.setRemoteDescription(new RTCSessionDescription(message));
-    doAnswer();
+	if (!isInitiator && !isStarted) {
+	  maybeStart();
+	}
+	pc.setRemoteDescription(new RTCSessionDescription(message));
+	doAnswer();
   } else if (message.type === 'answer' && isStarted) {
-    pc.setRemoteDescription(new RTCSessionDescription(message));
+	pc.setRemoteDescription(new RTCSessionDescription(message));
   } else if (message.type === 'candidate' && isStarted) {
-    var candidate = new RTCIceCandidate({
-      sdpMLineIndex: message.label,
-      candidate: message.candidate
-    });
-    pc.addIceCandidate(candidate);
+	var candidate = new RTCIceCandidate({
+	  sdpMLineIndex: message.label,
+	  candidate: message.candidate
+	});
+	pc.addIceCandidate(candidate);
   } else if (message === 'bye' && isStarted) {
-    handleRemoteHangup();
+	handleRemoteHangup();
   }
 });
 
@@ -282,7 +349,7 @@ function gotStream(stream) {
   localVideo.srcObject = stream;
   sendMessage('got user media');
   if (isInitiator) {
-    maybeStart();
+	maybeStart();
   }
 }
 
@@ -294,24 +361,25 @@ console.log('Getting user media with constraints', constraints);
 
 if (location.hostname !== 'localhost') {
   requestTurn(
-    'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
+	'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
   );
 }
 
 function maybeStart() {
   console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady);
   if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
-    console.log('>>>>>> creating peer connection');
-    createPeerConnection();
-    pc.addStream(localStream);
-    isStarted = true;
-    sendBtn.disabled = false;
-    snapBtn.disabled = false;
-    dataChannelSend.disabled = false;
-    console.log('isInitiator', isInitiator);
-    if (isInitiator) {
-      doCall();
-    }
+	console.log('>>>>>> creating peer connection');
+	createPeerConnection();
+	pc.addStream(localStream);
+	isStarted = true;
+	sendBtn.disabled = false;
+	snapBtn.disabled = false;
+	recordBtn.disabled = false;
+	dataChannelSend.disabled = false;
+	console.log('isInitiator', isInitiator);
+	if (isInitiator) {
+	  doCall();
+	}
   }
 }
 
@@ -323,36 +391,36 @@ window.onbeforeunload = function() {
 //      Initialization and Event about DataChannel     //
 /////////////////////////////////////////////////////////
 function initDataChannel(){
-    // 클라이언트로부터 전송해오는 DataChannel 콜백함수를 지정한다.
-    sendChannel = pc.createDataChannel('sendDataChannel', null);
-    console.log('Created send data channel');
-    
-    //전송을 위한 DataChannel의 콜백 지정
-    sendChannel.onopen = onSendChannelStateChange;
-    sendChannel.onclose = onSendChannelStateChange;
-    console.log('set callback function for sendChannel');
+	// 클라이언트로부터 전송해오는 DataChannel 콜백함수를 지정한다.
+	sendChannel = pc.createDataChannel('sendDataChannel', null);
+	console.log('Created send data channel');
+	
+	//전송을 위한 DataChannel의 콜백 지정
+	sendChannel.onopen = onSendChannelStateChange;
+	sendChannel.onclose = onSendChannelStateChange;
+	console.log('set callback function for sendChannel');
 
-    // 수신을 위한 DataChannel의 콜백 지정
-    pc.ondatachannel = receiveChannelCallback;
-    console.log('set callback function for receiveChannelCallback');
+	// 수신을 위한 DataChannel의 콜백 지정
+	pc.ondatachannel = receiveChannelCallback;
+	console.log('set callback function for receiveChannelCallback');
 }
 
 
 function createPeerConnection() {
   try {
-    pc = new RTCPeerConnection(null);
-    pc.onicecandidate = handleIceCandidate;
-    pc.onaddstream = handleRemoteStreamAdded;
-    pc.onremovestream = handleRemoteStreamRemoved;
+	pc = new RTCPeerConnection(null);
+	pc.onicecandidate = handleIceCandidate;
+	pc.onaddstream = handleRemoteStreamAdded;
+	pc.onremovestream = handleRemoteStreamRemoved;
 
-    initDataChannel();
-    
-    console.log('Created RTCPeerConnnection');
+	initDataChannel();
+	
+	console.log('Created RTCPeerConnnection');
 
   } catch (e) {
-    console.log('Failed to create PeerConnection, exception: ' + e.message);
-    alert('Cannot create RTCPeerConnection object.');
-    return;
+	console.log('Failed to create PeerConnection, exception: ' + e.message);
+	alert('Cannot create RTCPeerConnection object.');
+	return;
   }
 }
 
@@ -360,23 +428,23 @@ function onSendChannelStateChange() {
   var readyState = sendChannel.readyState;
   trace('Send channel state is: ' + readyState);
   if (readyState === 'open') {
-    console.log('readyState is open');
+	console.log('readyState is open');
   } else {
-    console.log('readyState is close');
+	console.log('readyState is close');
   }
 }
 
 function handleIceCandidate(event) {
   console.log('icecandidate event: ', event);
   if (event.candidate) {
-    sendMessage({
-      type: 'candidate',
-      label: event.candidate.sdpMLineIndex,
-      id: event.candidate.sdpMid,
-      candidate: event.candidate.candidate
-    });
+	sendMessage({
+	  type: 'candidate',
+	  label: event.candidate.sdpMLineIndex,
+	  id: event.candidate.sdpMid,
+	  candidate: event.candidate.candidate
+	});
   } else {
-    console.log('End of candidates.');
+	console.log('End of candidates.');
   }
 }
 
@@ -392,8 +460,8 @@ function doCall() {
 function doAnswer() {
   console.log('Sending answer to peer.');
   pc.createAnswer().then(
-    setLocalAndSendMessage,
-    onCreateSessionDescriptionError
+	setLocalAndSendMessage,
+	onCreateSessionDescriptionError
   );
 }
 
@@ -410,29 +478,29 @@ function onCreateSessionDescriptionError(error) {
 function requestTurn(turnURL) {
   var turnExists = false;
   for (var i in pcConfig.iceServers) {
-    if (pcConfig.iceServers[i].urls.substr(0, 5) === 'turn:') {
-      turnExists = true;
-      turnReady = true;
-      break;
-    }
+	if (pcConfig.iceServers[i].urls.substr(0, 5) === 'turn:') {
+	  turnExists = true;
+	  turnReady = true;
+	  break;
+	}
   }
   if (!turnExists) {
-    console.log('Getting TURN server from ', turnURL);
-    // No TURN server. Get one from computeengineondemand.appspot.com:
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var turnServer = JSON.parse(xhr.responseText);
-        console.log('Got TURN server: ', turnServer);
-        pcConfig.iceServers.push({
-          'urls': 'turn:' + turnServer.username + '@' + turnServer.turn,
-          'credential': turnServer.password
-        });
-        turnReady = true;
-      }
-    };
-    xhr.open('GET', turnURL, true);
-    xhr.send();
+	console.log('Getting TURN server from ', turnURL);
+	// No TURN server. Get one from computeengineondemand.appspot.com:
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState === 4 && xhr.status === 200) {
+		var turnServer = JSON.parse(xhr.responseText);
+		console.log('Got TURN server: ', turnServer);
+		pcConfig.iceServers.push({
+		  'urls': 'turn:' + turnServer.username + '@' + turnServer.turn,
+		  'credential': turnServer.password
+		});
+		turnReady = true;
+	  }
+	};
+	xhr.open('GET', turnURL, true);
+	xhr.send();
   }
 }
 
@@ -442,9 +510,9 @@ function handleRemoteStreamAdded(event) {
   remoteVideo.srcObject = remoteStream;
   // 스냅샵을 위한 비디오 전처리
   remoteVideo.onloadedmetadata = function(){
-    photo.width = photoContextW = remoteVideo.videoWidth;
-    photo.height = photoContextH = remoteVideo.videoHeight;
-    trace('got remoteStream with width and height : ', photoContextW, photoContextH);
+	photo.width = photoContextW = remoteVideo.videoWidth;
+	photo.height = photoContextH = remoteVideo.videoHeight;
+	trace('got remoteStream with width and height : ', photoContextW, photoContextH);
   }
 }
 
@@ -493,22 +561,22 @@ function onReceiveChannelStateChange() {
 ///////////////////////////////////////////////////////
 function trace(text) {
   if (text[text.length - 1] === '\n') {
-    text = text.substring(0, text.length - 1);
+	text = text.substring(0, text.length - 1);
   }
   if (window.performance) {
-    var now = (window.performance.now() / 1000).toFixed(3);
-    var d = new Date()
-            , yy = d.getFullYear()
-            , mm = leadingZeros(d.getMonth(),2)
-            , dd = leadingZeros(d.getDate(),2)
-            , h = d.getHours()
-            , m = d.getMinutes()
-            , s = leadingZeros(d.getSeconds(),2)
-            , ms = leadingZeros(d.getMilliseconds(),3);
-    var time = yy+'-'+mm+'-'+dd+' '+'['+h + ':' + m +':' + s + '.' + ms +']';
-    console.log(time + ' - ' + text);
+	var now = (window.performance.now() / 1000).toFixed(3);
+	var d = new Date()
+			, yy = d.getFullYear()
+			, mm = leadingZeros(d.getMonth(),2)
+			, dd = leadingZeros(d.getDate(),2)
+			, h = d.getHours()
+			, m = d.getMinutes()
+			, s = leadingZeros(d.getSeconds(),2)
+			, ms = leadingZeros(d.getMilliseconds(),3);
+	var time = yy+'-'+mm+'-'+dd+' '+'['+h + ':' + m +':' + s + '.' + ms +']';
+	console.log(time + ' - ' + text);
   } else {
-    console.log(text);
+	console.log(text);
   }
 }
 
@@ -517,8 +585,8 @@ function leadingZeros(n, digits) {
   n = n.toString();
 
   if (n.length < digits) {
-    for (var i = 0; i < digits - n.length; i++)
-      zero += '0';
+	for (var i = 0; i < digits - n.length; i++)
+	  zero += '0';
   }
   return zero + n;
 }
